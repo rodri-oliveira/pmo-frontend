acd26ebdcc1de3adbbcad1503e4172dc56f6b29a
Rodrigo de Oliveira <roliveira@weg.net>
2025-08-06 14:38:29 -0300

    build: Adiciona cache buster para forçar reconstrução

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        CACHE_BUSTER: process.env.CACHE_BUSTER,
+    },
 
     output: "standalone",
     async rewrites() {
